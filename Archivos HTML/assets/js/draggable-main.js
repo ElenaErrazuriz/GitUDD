6af72980f6b7273d95a65da79257ca3942693804
benjamin meneses <benjamin.meneses.14@sansano.usm.cl>
2019-10-04 13:41:09 -0300

    wow

--- Archivos HTML/assets/js/draggable-main.js
+++ Archivos HTML/assets/js/draggable-main.js
@@ -1,84 +1,17 @@
-var ramos = [
-  {
-    idramo: 1,
-    titulo: 'Arte y Cultura',
-    seccion: [
-      {
-        idseccion: 1,
-        token: 'vie@@h1&&jue@@h7'
-      },
-      {
-        idseccion: 2,
-        token: 'mie@@h6&&jue@@h3'
-      },
-      {
-        idseccion: 3,
-        token: 'mar@@h4&&sab@@h2'
-      },
-    ]
-  },
-  {
-    idramo: 2,
-    titulo: 'Programación',
-    seccion: [
-      {
-        idseccion: 1,
-        token: 'jue@@h1&&jue@@h6'
-      },
-      {
-        idseccion: 2,
-        token: 'vie@@h1&&mie@@h7'
-      },
-      {
-        idseccion: 3,
-        token: 'lun@@h5&&jue@@h8'
-      },
-    ]
-  },
-  {
-    idramo: 3,
-    titulo: 'Taller de Diseño web',
-    seccion: [
-      {
-        idseccion: 1,
-        token: 'lun@@h2&&mar@@h6'
-      },
-      {
-        idseccion: 2,
-        token: 'mie@@h5&&jue@@h7'
-      },
-      {
-        idseccion: 3,
-        token: 'vie@@h3&&vie@@h4'
-      },
-    ]
-  },
-  {
-    idramo: 4,
-    titulo: 'Gimnasia Artística',
-    seccion: [
-      {
-        idseccion: 1,
-        token: 'vie@@h2&&mar@@h7'
-      },
-      {
-        idseccion: 2,
-        token: 'mar@@h5&&vie@@h5'
-      },
-      {
-        idseccion: 3,
-        token: 'lun@@h3&&mar@@h1'
-      },
-    ]
-  },
-
-]
 var dragging = false
 var currentSimulation
 import('./postulacion.js').then((module) => {
   currentSimulation = new module.default()
+  currentSimulation.loadSimulationData()
   drawHorario()
+  setSelect()
 })
+function setSelect(){
+  let simulator = $('#simulatorSelect')
+  currentSimulation.getSimulationList().forEach(function(item) {
+    simulator.append('<option value="' + item.codramo + '">' + item.nombre +'</option>')
+  })
+}
 // función para permitir 'drop' en el horario
 function onDragOver (e) {
   e.preventDefault()
@@ -94,6 +27,18 @@ function delSection(idramo) {
 // Focusear itemes de la lista de ramos arrastrables
 function setFocus (idramo, idseccion) {
   $(".section-simulator-" + idramo + "-" + idseccion).addClass("section-selected")
+}
+function convertBloque (bloque) {
+  let conversorList = [{real: 'Lunes', abv: 'lun'},{real: 'Martes', abv: 'mar'},{real: 'Miércoles', abv: 'mie'},{real: 'Jueves', abv: 'jue'},{real: 'Viernes', abv: 'vie'},{real: 'Sábado', abv: 'sab'},{real: 'Domingo', abv: 'dom'},]
+  let abv = conversorList.filter(item => item.real === bloque.dia)[0].abv
+  let ini = bloque.inicio.split(':')
+  let fin = bloque.fin.split(':')
+  let duration = (parseInt(fin[0]) - parseInt(ini[0]))*60 + (parseInt(fin[1]) - parseInt(ini[1]))
+  return {
+    ...bloque,
+    abv: abv,
+    duration: duration,
+  }
 }
 // Desfocusear itemes de la lista de ramos arrastrables
 function deFocus (idramo) {
@@ -128,15 +73,15 @@ function drawHorario () {
   let ramos = currentSimulation.getList()
   let aux
   ramos.forEach(function (ramo) {
-    ramo.horarios.forEach((horario) => {
-      aux = $("#" + horario[1].toString() + " > ." + horario[0].toString())
+    ramo.bloques.forEach((bloque) => {
+      bloque = convertBloque(bloque)
+      aux = $("#" + bloque.inicio.split(':')[0] + " > ." + bloque.abv)
       aux.html(
-        '<span class="td-data-hour--content ' + 'ramo-' + ramo.idRamo + '">\n' +
+        '<span class="td-data-hour--content ' + 'ramo-' + ramo.codramo + '">\n' +
         '    <a href="#" class="td-data-hour--subject td-data-hour--subject-postulate">\n' +
         '      <span class="td-popover td-popover--postulate">\n' +
-        '        <span class="title-td-delete" onclick="delSection(' + ramo.idRamo + ')"> x </span>\n' +
-        '        <span class="title-td-subject"> ' + ramo.title + '</span>\n' +
-        '        <span class="title-td-section">Sección  ' + ramo.idSeccion + '</span>\n' +
+        '        <span class="title-td-delete" onclick="delSection(' + ramo.codramo + ')"> x </span>\n' +
+        '        <span class="title-td-subject"> ' + ramo.nombre + '</span>\n' +
         '      </span>\n' +
         '    </a>\n' +
         '</span>\n'
@@ -148,20 +93,21 @@ function drawHorario () {
 function drawList (newRamo) {
   var list = $("#simulatorList")
   list.html('')
-  var data = ramos.filter(function(item){
-    return parseInt(item.idramo) === parseInt(newRamo)
+  var data = currentSimulation.getSimulationList().filter(function(item){
+    return item.codramo === newRamo
   })
-  data[0].seccion.forEach(function (item) {
-    let availability = currentSimulation.checkAvailability(detokenize(item.token))
+  data[0].secciones.forEach(function (item) {
+    let availability = currentSimulation.checkAvailability(item.bloques)
     let html =
-      '<div class="horario2 section-simulator section-simulator-' + newRamo + '-' + item.idseccion + '" draggable="true" onmouseup="handleClick(this)" ondragstart="startdrag(event)" ondragend="undrag()" data-name="' + data[0].titulo + '" data-idramo="' + newRamo + '" data-idseccion="' + item.idseccion + '" data-token="' + item.token + '">\n' +
+      '<div class="horario2 section-simulator section-simulator-' + newRamo + '-' + item.idSeccion + '" draggable="true" onmouseup="handleClick(this)" ondragstart="startdrag(event)" ondragend="undrag()" data-idramo="' + newRamo + '" data-idseccion="' + item.idSeccion + '">\n' +
       '  <li class="td-data-hour--subject ' + (availability.isPosible ? 'td-data-hour--subject-postulate' : 'td-data-hour--subject-required') +' no-bullets">\n' +
       '    <span class="td-popover td-popover--postulate">\n' +
-      '      <span class="title-td-subject">' + data[0].titulo + '</span>\n' +
-      '      <span class="title-td-section">Sección ' + item.idseccion + '</span>\n' +
+      '      <span class="title-td-subject">' + item.codramo +  ' - ' + item.nombre + '</span>\n' +
+      '      <span class="title-td-section">Sección ' + (item.idSeccion) + '</span>\n' +
       '      <span class="td-data-hour--campus">\n';
-    detokenize(item.token).forEach(function (item) {
-      html += '        <span class="td-data-hour--campus-title">' + item[0] + ' - ' + item[1] + '</span>\n'
+
+     item.bloques.map(bloque => convertBloque(bloque)).forEach(function (item) {
+      html += '        <span class="td-data-hour--campus-title">' + item.dia + ' - ' + item.inicio + ' - ' + item.duration + '</span>\n'
     })
     html += '</span>\n' +
       '    </span>\n' +
@@ -170,49 +116,39 @@ function drawList (newRamo) {
     list.append(html)
   })
   var idSeccionFocus = currentSimulation.checkFocus(newRamo)
-  if(idSeccionFocus > 0){
+  debugger
+  if(idSeccionFocus !== '0'){
     setFocus(newRamo, idSeccionFocus)
   }
 }
 
 function handleClick (ev) {
   if (!dragging) {
-    var data = $(ev).data('token');
-    var title = $(ev).data('name');
     var idseccion = $(ev).data('idseccion');
     var idramo = $(ev).data('idramo');
 
-    data = detokenize(data);
-    // revisar si tiene choque de horarios
-    var availability = currentSimulation.checkAvailability(data)
-    if (availability.isPosible) {
-      delSection(idramo)
-      setFocus(idramo, idseccion)
-      currentSimulation.addRamo({
-        idRamo: idramo,
-        idSeccion: idseccion,
-        horarios: data,
-        title: title,
-      })
-    } else {
-      if(confirm("Se eliminarán " + availability.ocurrences.length + " ramos con tope, ¿Seguro que desea continuar?")){
+    let data = currentSimulation.getSeccionInfo(idramo, idseccion)
+    if(data !== false){
+      // revisar si tiene choque de horarios
+      var availability = currentSimulation.checkAvailability(data.bloques)
+      if (availability.isPosible) {
         delSection(idramo)
-        availability.ocurrences.forEach((ramo) => {
-          console.log(ramo)
-          delSection(ramo.idRamo)
-        })
         setFocus(idramo, idseccion)
-        currentSimulation.addRamo({
-          idRamo: idramo,
-          idSeccion: idseccion,
-          horarios: data,
-          title: title,
-        })
+        currentSimulation.addRamo(data)
+      } else {
+        if(confirm("Se eliminarán " + availability.ocurrences.length + " ramos con tope, ¿Seguro que desea continuar?")){
+          delSection(idramo)
+          availability.ocurrences.forEach((ramo) => {
+            delSection(ramo.idRamo)
+          })
+          setFocus(idramo, idseccion)
+          currentSimulation.addRamo(data)
+        }
       }
+      var newRamo = $('#simulatorSelect').val()
+      drawList(newRamo)
+      drawHorario()
     }
-    var newRamo = $('#simulatorSelect').val()
-    drawList(newRamo)
-    drawHorario()
   }
 }
 // Funcion que maneja el drop de los elementos en la tabla
